0eb30e6a0e32ae960f432f945bfba8f340baf6b9
Arling Holguin <arlingholguin@gmail.com>
2022-11-24 07:31:01 -0500

    Ajustes finales

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Gif, SearchGifsresponse } from '../interfaces/gif.Interfsces';
 
@@ -7,6 +7,7 @@ import { Gif, SearchGifsresponse } from '../interfaces/gif.Interfsces';
 })
 export class GifsService {
   private apiKey: string = 'YpByDd5ZRGCMFpoFW8NZnH9xcbcKGeeb';
+  private servicioUrl: string = 'http://api.giphy.com/v1/gifs';
   private _historial: string[] = [];
   public resultados: Gif[] = [];
 
@@ -26,14 +27,24 @@ export class GifsService {
     // }
   }
 
+
+
   buscarGifs(query:string = ''){
+
     query = query.trim().toLowerCase();
     if (!this._historial.includes(query)) {
       this._historial.unshift(query);
       this._historial = this._historial.splice(0,10);
       localStorage.setItem('historial', JSON.stringify(this._historial));
     }
-    this.http.get<SearchGifsresponse>(`http://api.giphy.com/v1/gifs/search?api_key=${ this.apiKey }&q=${ query }&limit=15`)
+
+    const params = new HttpParams()
+          .set('api_key', this.apiKey)
+          .set('limit', '10')
+          .set('q', query);
+
+
+    this.http.get<SearchGifsresponse>(`${this.servicioUrl}/search`, {params})
       .subscribe((resp) => {
         console.log(resp.data);
         this.resultados = resp.data;
